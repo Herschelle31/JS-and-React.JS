c28f9992572f26ca4b013aec2bb7c82d53faea4a
Herschelle <112215072@cse.iiitp.ac.in>
2024-08-15 23:42:16 +0530

    new folder

--- CurrencyConverter/src/components/index.js
+++
@@ -1,5 +0,0 @@
-//aa file ma badha components import as well as export kari devanu etle shu aapde App.jsx ma khali aa ekaj file import karvani thay
-
-import InputBox from "./InputBox";
-
-export {InputBox}
